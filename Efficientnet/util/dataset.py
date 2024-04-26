9c604af4c47ff85bc1eb79397058c7c813669f74
Mohammed Omer <mohammed.omer.77omr@gmail.com>
2024-04-26 11:44:40 +0530

    Update dataset.py

--- Efficientnet/util/dataset.py
+++ Efficientnet/util/dataset.py
@@ -7,33 +7,13 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from typing  import  Tuple, Dict, List
 
-def display_random_images(dataset: torch.utils.data.dataset.Dataset,
-                          classes:  bool = True,
-                          n: int = 5,
-                          display_shape: bool = False):
-  if n > 5:
-      n = 5
-      display_shape = False
-      print(f"For display purposes, n shouldn't be larger than 5, setting to 5 and removing shape display.")
-
-  random_samples_idx = random.sample(range(len(dataset)), k=n)
-  print(f"Format of labels: {dataset.attribs}")
-  plt.figure(figsize=(16, 10))
-
-  for i, targ_sample in enumerate(random_samples_idx):
-    targ_image, targ_label = dataset[targ_sample][0], dataset[targ_sample][1]
-    targ_label = dataset.decode_labels_onehot(targ_label)
-    targ_image_adjust = targ_image.clamp(0, 1).permute(1, 2, 0).cpu().numpy()
-    plt.subplot(1, n, i+1)
-    plt.imshow(targ_image_adjust)
-    plt.axis("off")
-    plt.rc('axes', titlesize=8)
-    if classes:
-        title = list(targ_label)
-        if display_shape:
-            title = title + f"\nshape: {targ_image_adjust.shape}"
-        plt.title(title)
-
+def create_balanced_df(df,cls,k):
+  def sampling_k_elements(group, k=k):
+    if len(group) < k:
+        return group
+    return group.sample(k)
+  return df.groupby(cls).apply(sampling_k_elements).reset_index(drop=True)
+  
 class CustomImageDataset(torch.utils.data.dataset.Dataset):
   def __init__(self, img_dir: str,attribs:List,
                item_2_label_lst:List,label_2_item_lst:List,
